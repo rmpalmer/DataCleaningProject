e24153ae76e2dce8fba6babb5481272b658c3f62
Richard M. Palmer <richard.m.palmer@gmail.com>
2017-02-19 15:56:41 -0600

    almost done

--- README.md
+++ README.md
@@ -4,5 +4,42 @@ End of course project by Richard M. Palmer
 
 ## run_analysis.R script
 
-## Code Book
+This script does the following:
 
+### load necessary libraries
+### Get a list of all features from the file features.txt
+### Read in all the measurements for the training data from the file
+X_train.txt
+### Read the subjects from subject_train.txt and append this to the measurements
+### Read the activity codes from y_train.txt and append this to the measurements
+### Read in all the measurements for the test data from the file
+X_test.txt
+### Read the subjects from subject_test.txt and append this to the measurements
+### Read the activity codes from y_test.txt and append this to the measurements
+### Row bind the training and test dataests together
+### Get a map of integer activity codes to label strings from the file
+activity_labels.txt
+### use join to add the activity labels from the map to each row
+### strip off the names of all columns
+### find the column indices of measurements to keep by grep on the
+vector of column names.  Also include the subject, activity label,
+and activity code columns
+### use the resulting column indices to select data to keep
+### again using grep, find the names of measurements of interest
+### melt the dataset into a narrow tidy frame with one measurement per row
+### use dcast to reshape the dataframe so that each row is a unique
+combination of activity and subject.  Use the mean function to collect
+all measurements for each such combination.
+### write out the resulting data frame as a text file.
+
+## Reslting data
+
+The resulting data are 180 rows with 68 columns.
+Each row represents a different combination of activity and subject.
+The first two columns give the activity label and subject.
+The remaining columns give the mean of the measurements for that
+particular combination of activity and subject.
+
+## The following command can be used to load the resulting data
+
+data <- read.table(file="result.txt",header=TRUE)
--- run_analysis.R
+++ run_analysis.R
@@ -4,24 +4,21 @@
 # https://github.com/rmpalmer/DataCleaningProject.git
 
 # for joining...
+library(plyr)
 library(dplyr)
 
-# for melt, dcast
+# for melt and dcast
 library(reshape2)
 
 # get a list of the features, used to assign column names when reading data
 features <- read.table(file="UCI HAR Dataset/features.txt"
                        ,colClasses = c("integer","character"))
 
-# get a mapping of activities (common to training and test)
-activity_map <- read.table("UCI HAR Dataset/activity_labels.txt"
-                          ,colClasses = c("integer","character")
-                          ,col.names = c("activity_code","activity_label"))
+# -- TRAINING --
 
 # read in training data, assigning feature names to the columns
 training <- read.table(file="UCI HAR Dataset/train/X_train.txt",col.names=features$V2)
 
-# -- TRAINING --
 # get the subjects (training)
 subjects <- read.table("UCI HAR Dataset/train/subject_train.txt",col.names = "subjects")
 
@@ -48,7 +45,13 @@ test <- cbind(test,subjects,activity_code)
 # merge the training and test data
 all_data <- rbind(training, test)
 
-# map the activity codes to labels
+# get a mapping of activities (common to training and test)
+# this will be used to give descriptive names to the activities
+activity_map <- read.table("UCI HAR Dataset/activity_labels.txt"
+                           ,colClasses = c("integer","character")
+                           ,col.names = c("activity_code","activity_label"))
+
+# map the activity codes to descriptive labels
 all_data <- join(all_data,activity_map,by="activity_code")
 
 # get all column names out
@@ -59,10 +62,10 @@ all_columns <- names(all_data)
 #  or one of the columns that I added in
 cols_to_keep <- grep ("(\\.mean\\.|\\.std\\.|subjects|activity_code|activity_label)",all_columns)
 
-# select the columns of interest
+# select only the columns of interest
 selected_data <- select(all_data,cols_to_keep)
 
-# what are the measurement statistics of interest
+# what are the measurement statistics of interest (ie, only dependent variables)
 measurements_of_interest <- grep("(\\.mean\\.|\\.std\\.)",all_columns,value=TRUE)
 
 # make it a narrow tidy dataset
@@ -70,5 +73,10 @@ molten <- melt(selected_data
               ,id=c("subjects","activity_label")
               ,measure.vars = measurements_of_interest)
 
-# reshape so that the measurements are all a function of sub
-result <- dcast(molten,activity_label + subjects ~ variable,mean)
+# reshape so that the measurements are all a function of activity and subject
+# take the mean of each measurement 
+result <- dcast(molten,activity_label + subjects ~ variable, mean)
+
+# write out the dataset as a text file for upload
+write.table(result,file="result.txt",row.names = FALSE)
+
